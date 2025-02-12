a9da909545f9a108977045294c9b52d08ab823d1
sunny chen <z5421256@ad.unsw.edu.au>
2025-02-13 01:37:10 +1100

    revamped main page colours and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="title">🚂 Sydney Traindle 🚂</div>
+        <div className="title"><h1><img id='t-logo' src="TfNSW_T.svg"></img>Sydney traindle</h1></div>
         <Game/>
       </div>
     </>
