c1140b5244b94fa98e5c52cfdb6d294c41892630
sorryvol <darcypower9@gmail.com>
2025-02-14 07:25:36 +1100

    added alternate background images to avoid repetitve patterns

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -7,7 +7,12 @@ class Background extends React.Component {
         let images = []
         //creates enough background images to tile on any height background without scaling image
         for (let i = 0; i < height*4; i += width*0.25){
-            images.push(<img src='rails.svg' className="background"></img>)
+            if (Math.random() > 0.5){
+                images.push(<img src='rails.svg' className="background"></img>);
+            }
+            else {
+                images.push(<img src='rails2.svg' className="background"></img>);
+            }
         }
         return <div className="background-container">{images}</div>;
     }
