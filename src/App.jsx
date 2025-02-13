5df9c16dd703a1d2551647c494f7795c83514186
sorryvol <darcypower9@gmail.com>
2025-02-14 06:31:06 +1100

    started work on background image

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,13 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="title"><h1><img id='t-logo' src="TfNSW_T.svg"></img>Sydney Traindle</h1></div>
-        <Game/>
+        <div className="title">
+          <h1>
+            <img id='t-logo' src="TfNSW_T.svg"></img> Sydney Traindle
+          </h1>
+        </div>
         <Footer/>
+        <Game/>
         <Background/>
       </div>
     </>
--- src/components/background.jsx
+++ src/components/background.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-//make footer take props of image+link pairs and render them in a small div
-function Background(props) {
-    return (
-        <div className="background">
-
-        </div>
-    );
-};
-
+class Background extends React.Component {
+    render() {
+        let width = document.documentElement.clientWidth;
+        let height = document.documentElement.clientHeight;
+        let images = []
+        //creates enough background images to tile on any height background without scaling image
+        for (let i = 0; i < height*4; i += width*0.25){
+            images.push(<img src='rails.svg' className="background"></img>)
+        }
+        return <div className="background-container">{images}</div>;
+    }
+  }
 
 export default Background;
