377a2d8663e63c622c3948ac682265265fff9608
Hemang Punyane <hpun6804@uni.sydney.edu.au>
2025-02-13 22:32:00 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="title"><h1><img id='t-logo' src="TfNSW_T.svg"></img>Sydney traindle</h1></div>
+        <div className="title"><h1><img id='t-logo' src="TfNSW_T.svg"></img>Sydney Traindle</h1></div>
         <Game/>
       </div>
     </>
