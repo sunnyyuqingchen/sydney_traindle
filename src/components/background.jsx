933d90840de8c89bf14a80d2e1811ab220685a8b
sorryvol <darcypower9@gmail.com>
2025-02-14 20:50:29 +1100

    Adjusted some background settings and added content to meta tag

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -6,7 +6,7 @@ class Background extends React.Component {
         let height = document.documentElement.clientHeight;
         let images = []
         //creates enough background images to tile on any height background without scaling image
-        for (let i = 0; i < height*4; i += width*0.25){
+        for (let i = 0; i < width*5; i += width*0.20){
             if (Math.random() > 0.5){
                 images.push(<img src='rails.svg' className="background"></img>);
             }
