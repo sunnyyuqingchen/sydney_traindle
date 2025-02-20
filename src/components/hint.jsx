29b151448f068980bf8520470a74219371650c6a
sunny chen <z5421256@ad.unsw.edu.au>
2025-02-20 23:11:39 +1100

    made map zoomable

--- src/components/hint.jsx
+++ src/components/hint.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const hint = () => {
     const [isHint, setHint] = useState(false);
@@ -13,7 +14,16 @@ const hint = () => {
             {isHint && (
                 <>
                     <div className="map-container">
-                        <img id="map" src=".\map.png" alt="map" />
+                        <TransformWrapper
+                        initialScale={1} // Start at normal size
+                        minScale={1}     // No zooming out
+                        maxScale={5}     // Allow zoom up to 5x
+                        >
+                            {/* Image with Drag & Zoom */}
+                            <TransformComponent>
+                                <img id="map" src=".\map.png" alt="map" />
+                            </TransformComponent>
+                        </TransformWrapper>
                         <button id="closeHint" onClick={handleHint}><i class="bi bi-x-lg"></i></button>
                     </div>
                 </>
